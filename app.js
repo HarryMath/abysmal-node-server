6f70396bdcde985822e69728d3ba1cecb8ff8bb5
RedmiBook <nikitabort22092000@gmail.com>
2021-08-30 13:17:50 +0300

    created http endpoint for not crashing app on heroku

--- app.js
+++ app.js
@@ -5,54 +5,58 @@ const port = parseInt(process.env.PORT) || 3000;
 const host = '127.0.0.1';
 const sockets = [];
 
-http.createServer((request, response) => {
+const httpServer = http.createServer((request, response) => {
     const ipAddress = request.connection.localAddress;
     response.setHeader('Content-Type', 'text/plain');
     response.end(ipAddress);
-}).listen(port, () => {
+});
+httpServer.listen(port, () => {
     console.log('HTTP Server is running on port ' + port + '.');
 })
+setTimeout(createTcpServer, 3000);
 
-const tcpServer = net.createServer();
-tcpServer.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port + '.');
-});
-
-tcpServer.on('connection', function(socket) {
-    socket.setEncoding('binary');
-    console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
-    sockets.push(socket);
-    let data = '';
-    socket.on('data', function(bytes) {
-        data += bytes;
-        if (bytes.includes('}')) {
-            handleData(data);
-            data = data.split('}')[1];
-        }
+function createTcpServer() {
+    httpServer.close();
+    const tcpServer = net.createServer();
+    tcpServer.listen(port, host, () => {
+        console.log('TCP Server is running on port ' + port + '.');
     });
+    tcpServer.on('connection', function(socket) {
+        socket.setEncoding('binary');
+        console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+        sockets.push(socket);
+        let data = '';
+        socket.on('data', function(bytes) {
+            data += bytes;
+            if (bytes.includes('}')) {
+                handleData(data);
+                data = data.split('}')[1];
+            }
+        });
 
-    const handleData = (data) => {
-        try {
-            const object = JSON.parse(data);
-            sockets.forEach(s => {
-                if (s.remoteAddress !== socket.remoteAddress || s.remotePort !== socket.remotePort) {
-                    s.write(data);
-                }
-            });
-        } catch (ignore) {
-            console.log('error: ' + data);
-            console.log(ignore);
+        const handleData = (data) => {
+            try {
+                const object = JSON.parse(data);
+                sockets.forEach(s => {
+                    if (s.remoteAddress !== socket.remoteAddress || s.remotePort !== socket.remotePort) {
+                        s.write(data);
+                    }
+                });
+            } catch (ignore) {
+                console.log('error: ' + data);
+                console.log(ignore);
+            }
         }
-    }
 
-    const handleDisconnect = () => {
-        let index = sockets.findIndex(function(o) {
-            return o.remoteAddress === socket.remoteAddress && o.remotePort === socket.remotePort;
-        })
-        if (index !== -1) sockets.splice(index, 1);
-        console.log('players amount: ' + sockets.length);
-    }
+        const handleDisconnect = () => {
+            let index = sockets.findIndex(function(o) {
+                return o.remoteAddress === socket.remoteAddress && o.remotePort === socket.remotePort;
+            })
+            if (index !== -1) sockets.splice(index, 1);
+            console.log('players amount: ' + sockets.length);
+        }
 
-    socket.on('error', handleDisconnect);
-    socket.on('close', handleDisconnect);
-});
+        socket.on('error', handleDisconnect);
+        socket.on('close', handleDisconnect);
+    });
+}
