b4595f354260f8f839c7cc9823e5f90fa56ee4a5
RedmiBook <nikitabort22092000@gmail.com>
2021-09-12 22:00:29 +0300

    enemies are processing correctly

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 
 EXPOSE 4380
 ENV PORT=4380
-CMD [ "node", "serverDocker.js" ]
+CMD [ "node", "server.js" ]
---
+++ sizeOf.js
@@ -0,0 +1,23 @@
+const sizeOf = (object) => {
+    const objectList = [];
+    const stack = [object];
+    let bytes = 0;
+    while (stack.length) {
+        const value = stack.pop();
+        if (typeof value === 'boolean') {
+            bytes += 4;
+        } else if (typeof value === 'string') {
+            bytes += value.length * 2;
+        } else if (typeof value === 'number') {
+            bytes += 8;
+        } else if (typeof value === 'object' && objectList.indexOf(value) === -1) {
+            objectList.push(value);
+            for (let prop in value) {
+                stack.push(value[prop]);
+            }
+        }
+    }
+    return bytes;
+}
+
+module.exports = {sayHi: sizeOf};
